54028ee067b3e1c72fe8d6e4402e376d5dd5a981
Gerry Kyalo <54371756+GerrayJr@users.noreply.github.com>
2021-01-04 12:47:53 +0300

    1st commit FacilityLocation = got location

--- app/src/main/java/com/gerray/fmsystem/ManagerModule/FacilityLocation.java
+++ app/src/main/java/com/gerray/fmsystem/ManagerModule/FacilityLocation.java
@@ -1,37 +1,39 @@
 package com.gerray.fmsystem.ManagerModule;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.gerray.fmsystem.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -42,11 +44,26 @@ import com.karumi.dexter.listener.single.PermissionListener;
 public class FacilityLocation extends AppCompatActivity {
 
     private FusedLocationProviderClient client;
+    private EditText edLat, edLong;
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference firebaseDatabaseReference;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facility_location);
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Locations");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabaseReference = firebaseDatabase.getReference();
+        progressDialog = new ProgressDialog(this);
 
         client = LocationServices.getFusedLocationProviderClient(this);
         Button btnCord = findViewById(R.id.btnGetCord);
@@ -59,9 +76,40 @@ public class FacilityLocation extends AppCompatActivity {
                         @Override
                         public void onSuccess(Location location) {
                             if (location != null) {
-                                double latitude = location.getLatitude();
-                                double longitude = location.getLongitude();
-                                Toast.makeText(FacilityLocation.this, "Latitude: " + latitude + " Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+                                progressDialog.show();
+                                final double locLatitude = location.getLatitude();
+                                final double locLongitude = location.getLongitude();
+                                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                                if (firebaseUser != null) {
+                                    firebaseDatabaseReference.child("Facilities").child(firebaseUser.getUid()).child("Profile")
+                                            .addValueEventListener(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    String facilityName = null, facilityType = null, facilityEmail = null;
+                                                    if (snapshot.child("facilityName").exists()) {
+                                                        facilityName = snapshot.child("facilityName").getValue().toString();
+                                                    }
+                                                    if (snapshot.child("facilityType").exists()) {
+                                                        facilityType = snapshot.child("facilityType").getValue().toString();
+                                                    }
+                                                    if (snapshot.child("emailAddress").exists()) {
+                                                        facilityEmail = snapshot.child("emailAddress").getValue().toString();
+                                                    }
+
+                                                    LocationClass locationClass = new LocationClass(facilityName, facilityType, facilityEmail, locLatitude, locLongitude);
+                                                    databaseReference.child(firebaseUser.getUid()).setValue(locationClass);
+
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(FacilityLocation.this, "Saved", Toast.LENGTH_SHORT).show();
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                                }
+                                            });
+                                }
+                                Toast.makeText(FacilityLocation.this, "Latitude: " + locLatitude + " Longitude: " + locLongitude, Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
@@ -72,7 +120,6 @@ public class FacilityLocation extends AppCompatActivity {
                                 @Override
                                 public void onPermissionGranted(PermissionGrantedResponse response) {
                                     if (ContextCompat.checkSelfPermission(FacilityLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                                        //get current location coordinates
                                         client.getLastLocation().addOnSuccessListener(FacilityLocation.this, new OnSuccessListener<Location>() {
                                             @Override
                                             public void onSuccess(Location location) {
@@ -118,6 +165,54 @@ public class FacilityLocation extends AppCompatActivity {
                 }
             }
         });
+
+        edLat = findViewById(R.id.locLatitude);
+        edLong = findViewById(R.id.locLongitude);
+
+        Button btngetCord = findViewById(R.id.btnInputCord);
+        btngetCord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertLocation();
+            }
+        });
+
+    }
+
+    private void insertLocation() {
+        progressDialog.show();
+        final double latitude = Double.parseDouble(edLat.getText().toString().trim());
+        final double longitude = Double.parseDouble(edLong.getText().toString().trim());
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            firebaseDatabaseReference.child("Facilities").child(firebaseUser.getUid()).child("Profile")
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String facilityName = null, facilityType = null, facilityEmail = null;
+                            if (snapshot.child("facilityName").exists()) {
+                                facilityName = snapshot.child("facilityName").getValue().toString();
+                            }
+                            if (snapshot.child("facilityType").exists()) {
+                                facilityType = snapshot.child("facilityType").getValue().toString();
+                            }
+                            if (snapshot.child("emailAddress").exists()) {
+                                facilityEmail = snapshot.child("emailAddress").getValue().toString();
+                            }
+
+                            LocationClass locationClass = new LocationClass(facilityName, facilityType, facilityEmail, latitude, longitude);
+                            databaseReference.child(firebaseUser.getUid()).setValue(locationClass);
+
+                            progressDialog.dismiss();
+                            Toast.makeText(FacilityLocation.this, "Saved", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
 
 
     }
---
+++ app/src/main/java/com/gerray/fmsystem/ManagerModule/LocationClass.java
@@ -0,0 +1,61 @@
+package com.gerray.fmsystem.ManagerModule;
+
+public class LocationClass {
+    private String facilityName;
+    private String facilityType;
+    private String email;
+    private double latitude;
+    private double longitude;
+
+    public LocationClass() {
+    }
+
+    public LocationClass(String facilityName, String facilityType, String email, double latitude, double longitude) {
+        this.facilityName = facilityName;
+        this.facilityType = facilityType;
+        this.email = email;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+
+    public String getFacilityName() {
+        return facilityName;
+    }
+
+    public void setFacilityName(String facilityName) {
+        this.facilityName = facilityName;
+    }
+
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    public void setFacilityType(String facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
