60850bfc51eb54b74968d0fd9f4145e2a6637b91
Gerry Kyalo <54371756+gerrayjr@users.noreply.github.com>
2021-01-04 11:22:26 +0300

    1st commit FacilityLocation = got location

--- app/src/main/java/com/gerray/fmsystem/ManagerModule/FacilityLocation.java
+++ app/src/main/java/com/gerray/fmsystem/ManagerModule/FacilityLocation.java
@@ -1,16 +1,124 @@
 package com.gerray.fmsystem.ManagerModule;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.gerray.fmsystem.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class FacilityLocation extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facility_location);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        Button btnCord = findViewById(R.id.btnGetCord);
+        btnCord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(FacilityLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //get current location coordinates
+                    client.getLastLocation().addOnSuccessListener(FacilityLocation.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+                                Toast.makeText(FacilityLocation.this, "Latitude: " + latitude + " Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Dexter.withActivity(FacilityLocation.this)
+                            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                            .withListener(new PermissionListener() {
+                                @Override
+                                public void onPermissionGranted(PermissionGrantedResponse response) {
+                                    if (ContextCompat.checkSelfPermission(FacilityLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                        //get current location coordinates
+                                        client.getLastLocation().addOnSuccessListener(FacilityLocation.this, new OnSuccessListener<Location>() {
+                                            @Override
+                                            public void onSuccess(Location location) {
+                                                if (location != null) {
+                                                    double latitude = location.getLatitude();
+                                                    double longitude = location.getLongitude();
+                                                    Toast.makeText(FacilityLocation.this, "Latitude: " + latitude + " Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+
+                                @Override
+                                public void onPermissionDenied(PermissionDeniedResponse response) {
+                                    if (response.isPermanentlyDenied()) {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(FacilityLocation.this);
+                                        builder.setTitle("Permission Denied")
+                                                .setMessage("Permission to access Device's location is Permanently denied. Go to settings to allow permission")
+                                                .setNegativeButton("Cancel", null)
+                                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        Intent intent = new Intent();
+                                                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                        intent.setData(Uri.fromParts("Package", getPackageName(), null));
+
+                                                    }
+                                                })
+                                                .show();
+
+                                    } else {
+                                        Toast.makeText(FacilityLocation.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                    token.continuePermissionRequest();
+                                }
+                            })
+                            .check();
+                }
+            }
+        });
+
+
     }
 }
